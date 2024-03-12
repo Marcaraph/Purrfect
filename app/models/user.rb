617874d181502c26a3ad44d217868797663ae9af
Damien-Generet <damiengeneret@outlook.com>
2024-03-12 17:31:30 +0100

    add has_one cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one :cart       
   # belongs_to :cart,
   # has_many :items, through: :cart
 
