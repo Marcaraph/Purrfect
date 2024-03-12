af0d73393b0a2ec94da9d50493cc3c16145b22ed
Damien-Generet <damiengeneret@outlook.com>
2024-03-12 15:44:26 +0100

    Table order, order_item, seed, relation for cart order item user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,52 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+OrderItem.destroy_all
+Order.destroy_all
 CartItem.destroy_all
 Cart.destroy_all
 Item.destroy_all
 User.destroy_all
 
-#créaion de 20 items
+
+#création de 20 items
 20.times do
   Item.create(title: Faker::Lorem.sentence(word_count: 4), description: Faker::Lorem.sentence(word_count: 40), price: rand(10..50), image_url: "https://media.istockphoto.com/id/508030340/fr/photo/sunny-chat.jpg?s=2048x2048&w=is&k=20&c=Pwg60DIr0iYbQLwDXCHfdhfUgRKq7UdQsq6bfyAIaGA=")
 end
 
-#créaion de 10 users
-10.times do
+#création de 20 users
+20.times do
   password = "ssehkjgspouifghsi888"
   password_confirmation = password
   user = User.new(email: Faker::Internet.email, password: password, password_confirmation: password_confirmation)
   user.save!
 end
 
+#création d'un cart pour chaque user
 User.all.each do |user|
   Cart.create(user: user, price: 0)
 end
 
+#création de 10 cart items
 10.times do
   item = Item.all.sample
-  CartItem.create(cart: Cart.all.sample, item: item, price: item.price)
+  cart = Cart.all.sample
+  cart_item = CartItem.create(cart: cart, item: item, price: item.price)
+  Cart.find(cart.id).update(price: cart.price + cart_item.price)
+end
+
+#création de 10 orders
+10.times do 
+  cart = Cart.all.sample
+  cart_items = CartItem.where(cart: cart)
+  puts cart_items
+  #CREATION OF ORDER ( it's a cart already paid)
+  order = Order.create(user: cart.user, price: cart.price)
+  Cart.update(user: cart.user, price: 0)
+  CartItem.delete_by(cart_id: cart)
+
+  # CREATION OF ORDER_ITEMS ( History of all orders with link Items)
+  cart_items.each do |cart_item|
+    OrderItem.create(item: cart_item.item, order: order, price: cart_item.price)
+  end
 end
