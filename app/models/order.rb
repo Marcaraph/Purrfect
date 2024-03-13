3846c04878c53b7e7bda7eb412677dc0fec846d1
THOMAS AUDEVIE <thomasaudevie@thomass-mbp.home>
2024-03-13 14:32:04 +0100

    sent emails to the customer and the admin after an order is confirmed

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,11 @@ class Order < ApplicationRecord
     belongs_to :user
     has_many :order_items
     has_many :items, through: :order_items
+
+    after_create :send_order_emails
+
+    def send_order_emails
+        UserMailer.user_order_email(self.user).deliver_now
+        UserMailer.admin_order_email.deliver_now
+    end 
 end
